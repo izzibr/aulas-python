1865c3352feaa0b055d0739143f66905ae0996e0
Ivanise Oliveira <ivanise.oly@gmail.com>
2025-02-09 17:42:24 -0300

    Aulas 02

---
+++ strings.py
@@ -0,0 +1,33 @@
+# string entre ' ' ou " "
+
+s = "Sou uma string"
+print (type(s))
+
+nome = "Ivanise"
+print (nome == "Ivanise")
+
+#Concatenação de strings
+primeiro = "Ivanise"
+segundo = "Oliveira"
+print (primeiro + " " + segundo)
+
+#Multiplicação de strings, inteiro * string
+#não pode multiplicar duas strings
+a = "-"*3
+print (a)
+
+b = "A"*4
+print (b)
+
+#in para perguntar se tem alguma letra numa string
+print ("N" in primeiro)
+print ("n" in primeiro)
+
+
+# len() comprimento da string
+print (len (primeiro))
+print (f"O nome {primeiro} tem {len(primeiro)} letras")
+
+
+
+
